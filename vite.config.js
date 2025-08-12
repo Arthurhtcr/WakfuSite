716cee824706a69163a3bb32e6f03811b20d3971
Arthur Hautecoeur <arthur.hautecoeur@etu.univ-littoral.fr>
2025-08-12 11:43:51 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/WakfuSite/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,4 +14,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // ⬇️ Ajoute cette ligne pour GitHub Pages
+  base: process.env.NODE_ENV === 'production' ? '/WakfuSite/' : '/',
 })
